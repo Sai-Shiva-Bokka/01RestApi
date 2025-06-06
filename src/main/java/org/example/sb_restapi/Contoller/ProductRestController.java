02b34509ed98875ef3850c5e8f1953f9a7a487d1
S.S <bokkasaishiva@gmail.com>
2025-06-06 02:15:22 -0500

    Rest Api

--- src/main/java/org/example/sb_restapi/Contoller/ProductRestController.java
+++ src/main/java/org/example/sb_restapi/Contoller/ProductRestController.java
@@ -1,6 +1,7 @@
 package org.example.sb_restapi.Contoller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,21 @@ public class ProductRestController {
 
     @GetMapping("/product")
     public String getProductInfo(@RequestParam("id") Integer id) {
+        String msg = "";
+
+        if(id==101)
+            msg="Apple";
+        else if (id == 102) {
+            msg="Samsung";
+        }
+        else {
+            msg="No Product";
+        }
+        return msg;
+    }
+
+    @GetMapping("/product/{id}")
+    public String getProductInFo(@PathVariable("id") Integer id) {
         String msg = "";
 
         if(id==101)
